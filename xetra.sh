1d284026003df8549c335d7716e00003ad743d79
X E T R A <wtfxetra@gmail.com>
2025-11-01 13:58:07 +0530

    Create xetra.sh for system configuration setup
    Initialize locale, hostname, and hosts configuration.

---
+++ xetra.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+touch /etc/hostname
+# Create the /etc/hosts file. This is very important because it will resolve the listed hostnames locally and not over Internet DNS.
+touch /etc/hosts
+#Write the following ip, hostname pairs inside /etc/hosts, replacing Arch with YOUR hostname:
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 archlinux" >> /etc/hosts
+echo "archlinux" > /etc/hostname
+
+
+
